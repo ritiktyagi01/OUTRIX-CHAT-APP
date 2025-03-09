c80844c2d409e2581cf8ad10add4aa8ba00b6d66
Harsh sandhu <sandhuharsh9466@gmail.com>
2025-03-09 18:23:59 +0530

    vercel

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -11,7 +11,7 @@ function App() {
     
     <div className="App">
     <Routes>
-<Route path='/'  element={<Homepage/>} ></Route>
+<Route path='/'  element={<Homepage/>} ></Route>  
 <Route path = '/chat' element={<Chat/>}></Route>
 <Route path = '/signup' element={<Signup/>}></Route>
 <Route path = '/login' element={<Login/>}></Route>
